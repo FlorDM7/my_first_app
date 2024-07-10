d88b90449501d25a7629f6223a5ad5f6a0cf9460
Flor De Meulemeester <flor.de.meulemeester@gmail.com>
2024-07-10 13:24:15 +0200

    Added option to edit and complete tasks

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -44,10 +44,10 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   Tasklist tasklist = Tasklist(); // create new tasklist object
 
-  // method that makes a pop up for adding a new task
+  // method that makes a pop up widget for adding a new task
   Future<void> _showAddTaskDialog() async {
-    String _newTaskTitle = "Unnamed task"; // variables for the new task
-    String _newTaskDescription = "You didn't enter anything";
+    String _newTaskTitle = "Untitled task"; // variables for the new task
+    String _newTaskDescription = "";
 
     return showDialog(
       context: context,
@@ -60,7 +60,7 @@ class _MyHomePageState extends State<MyHomePage> {
               children: <Widget>[
                 TextField(
                   decoration: const InputDecoration(
-                    border: UnderlineInputBorder(), // place to enter text
+                    border: UnderlineInputBorder(), // place to get input from user
                     label: Text("Enter a title"),
                   ),
                   onChanged: (value) {
@@ -69,7 +69,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
                 TextField(
                   decoration: const InputDecoration(
-                    border: UnderlineInputBorder(), // place to enter text
+                    border: UnderlineInputBorder(), // place to get input from user
                     label: Text("Enter a description"),
                   ),
                   onChanged: (value) {
@@ -79,40 +79,185 @@ class _MyHomePageState extends State<MyHomePage> {
               ],
             ),
           ),
-          actions: <Widget>[ // two buttons at the bottom of the widget
+          actions: <Widget>[
+            // two 'action' buttons at the bottom of the widget
+            // cancel button
             TextButton(
-              style: TextButton.styleFrom(textStyle: Theme.of(context).textTheme.labelLarge,),
-              onPressed: () {
-                Navigator.of(context).pop(); // when button is pressed return to home screen
-              },
-              child: const Text("Cancel")),
+                style: TextButton.styleFrom(
+                  textStyle: Theme.of(context).textTheme.labelLarge,
+                ),
+                onPressed: () {
+                  Navigator.of(context)
+                      .pop(); // when button is pressed return to home screen
+                },
+                child: const Text("Cancel")),
+            // add button
             TextButton(
-              style: TextButton.styleFrom(textStyle: Theme.of(context).textTheme.labelLarge,),
-              onPressed: () {
-                Navigator.of(context).pop();
-                _addTask(_newTaskTitle, _newTaskDescription); // press add, call addTask method
-              },
-              child: const Text("Add")),
+                style: TextButton.styleFrom(
+                  textStyle: Theme.of(context).textTheme.labelLarge,
+                ),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  // press add, call addTask method
+                  _addTask(_newTaskTitle, _newTaskDescription);
+                },
+                child: const Text("Add")),
           ],
         );
       },
     );
   }
 
+  // add a new task
+  // by changing the state
   void _addTask(String title, String description) {
     setState(() {
       tasklist.addTask(Task.noDate(title, description));
     });
   }
 
+
+  // method that makes a pop up widget for an existing task, 
+  // with the option to edit or complete/delete the task
+  Future<void> _showInfoTaskDialog(Task currentTask) async {
+    return showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text(currentTask.getTitle()),
+            content: Text(currentTask.getDescription()),
+            actions: <Widget>[
+              // return button
+              TextButton(
+                  style: TextButton.styleFrom(
+                    textStyle: Theme.of(context).textTheme.labelLarge,
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                    // when button is pressed return to home screen
+                  },
+                  child: const Text("Return")),
+              // edit button
+              TextButton(
+                style: TextButton.styleFrom(
+                  textStyle: Theme.of(context).textTheme.labelLarge,
+                ),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  _showEditTaskDialog(currentTask);
+                },
+                child: const Text("Edit"),
+              ),
+              // complete button
+              TextButton(
+                style: TextButton.styleFrom(
+                  textStyle: Theme.of(context).textTheme.labelLarge,
+                ),
+                onPressed: () {
+                  _deleteTask(currentTask);
+                  Navigator.of(context).pop();
+                },
+                child: const Text("Complete"),
+              ),
+            ],
+          );
+        });
+  }
+
+  // delete a existing task
+  // by changing the state
+  void _deleteTask(Task task) {
+    setState(() {
+      tasklist.removeTask(task);
+      // TODO add to the archive
+    });
+  }
+
+
+  // method that makes a pop up widget for an exitsing task
+  // with the option to edit the title and description
+  Future<void> _showEditTaskDialog(Task currentTask) async {
+    String _newTaskTitle = currentTask.getTitle();
+    String _newTaskDescription = currentTask.getDescription();
+
+    return showDialog(
+      context: context,
+      barrierDismissible: false, // user must tap button to dismiss
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Edit '${currentTask.getTitle()}'"),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                TextField(
+                  decoration: const InputDecoration(
+                    border: UnderlineInputBorder(), // place to enter text
+                    label: Text("Enter a new title (optional)"),
+                  ),
+                  onChanged: (value) {
+                    _newTaskTitle = value; // change the local variable
+                  },
+                ),
+                TextField(
+                  decoration: const InputDecoration(
+                    border: UnderlineInputBorder(), // place to enter text
+                    label: Text("Enter a new description (optional)"),
+                  ),
+                  onChanged: (value) {
+                    _newTaskDescription = value; // change the local variable
+                  },
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            // two buttons at the bottom of the widget
+            // cancel button
+            TextButton(
+                style: TextButton.styleFrom(
+                  textStyle: Theme.of(context).textTheme.labelLarge,
+                ),
+                onPressed: () {
+                  // when button is pressed return to home screen
+                  Navigator.of(context).pop(); 
+                },
+                child: const Text("Cancel")),
+            // confirm button
+            TextButton(
+                style: TextButton.styleFrom(
+                  textStyle: Theme.of(context).textTheme.labelLarge,
+                ),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  // press confirm, call editTask method
+                  _editTask(currentTask, _newTaskTitle, _newTaskDescription);
+                },
+                child: const Text("Confirm")),
+          ],
+        );
+      },
+    );
+  }
+
+  // edit an existing task
+  // by changing the state
+  void _editTask(Task task, String title, String description) {
+    setState(() {
+      task.setTitle(title);
+      task.setDescription(description);
+    });
+  }
+
+  // This build method is rerun every time setState is called.
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called.
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
+      // List framework
       body: tasklist.isEmpty()
           ? const Center(child: Text('There are no tasks. Create one!'))
           : ListView.builder(
@@ -121,9 +266,13 @@ class _MyHomePageState extends State<MyHomePage> {
                 return ListTile(
                   title: Text(tasklist.getTaskAt(index).getTitle()),
                   subtitle: Text(tasklist.getTaskAt(index).getDescription()),
+                  onTap: () {
+                    _showInfoTaskDialog(tasklist.getTaskAt(index));
+                  },
                 );
               },
             ),
+      // Button to add new task
       floatingActionButton: FloatingActionButton(
         onPressed: _showAddTaskDialog,
         tooltip: 'Increment',
